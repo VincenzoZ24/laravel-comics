7da48ddeab77526487e6c52cfb66c6d0e150d773
VincenzoZ24 <zitov287@gmail.com>
2023-01-16 12:22:53 +0100

    modifiche stile

--- resources/views/guest/comics.blade.php
+++ resources/views/guest/comics.blade.php
@@ -1,13 +1,13 @@
 @extends('guest.layouts.base')
 @section('content')
-   @foreach ($comics as $comic)
+  
     <div class="card">
-        
+        @foreach ($comics as $comic)
         <div class="contents">
           <img class="img_card" src="{{$comic['thumb']}}" alt="" />
           <h5>{{ $comic['series']}}</h5>
         </div>
-        
+        @endforeach
       </div>
-      @endforeach
+      
 @endsection
